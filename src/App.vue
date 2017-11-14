62e7163a6b5219c33361b480c96b4bad64eddea4
zhongxiangbin <956574417@qq.com>
2017-11-14 13:05:02 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div id="c"></div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  #c{
+  	width: 100px;
+  	height: 100px;
+  	background: #42B983;
+  }
+}
+</style>
