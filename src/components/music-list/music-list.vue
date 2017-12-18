7ca3b0487dfd2f0344a2bcf9a134e2fb29adb854
zhongxiangbin <956574417@qq.com>
2017-12-18 18:38:26 +0800

    2017/12/18

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,12 +5,33 @@
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-
+      <div class="play-wrapper">
+        <div class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
+    <div class="bg-layer" ref="layer"></div>
+    <scroll :data="songs"
+            @scroll="scroll"
+            :listen-scroll="listenScroll"
+            :probe-type="probeType"
+            class="list"
+            ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+
   export default {
     props: {
       bgImage: {
@@ -32,12 +53,28 @@
       }
     },
     created() {
+      this.probeType = 3
+      this.listenScroll = true
       console.log(this.title)
+    },
+    mounted() {
+      this.imageHeight = this.$refs.bgImage.clientHeight
+      this.$refs.list.$el.style.top = `${this.imageHeight}px`
     },
     methods: {
       back() {
         this.$router.back()
+      },
+      scroll() {
+        // this.scrollY = pos.y
+      },
+      selectItem(item, index) {
+
       }
+    },
+    components: {
+      SongList,
+      Scroll
     }
   }
 </script>
@@ -65,6 +102,69 @@
         font-size: $font-size-large-x
         color: $color-theme
     .title
-
-
+      position: absolute
+      top: 0
+      left: 10%
+      z-index: 40
+      width: 80%
+      no-wrap()
+      text-align: center
+      line-height: 40px
+      font-size: $font-size-large
+      color: $color-text
+    .bg-image
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 70%
+      transform-origin: top
+      background-size: cover
+      .play-wrapper
+        position: absolute
+        bottom: 20px
+        z-index: 50
+        width: 100%
+        .play
+          box-sizing: border-box
+          width: 135px
+          padding: 7px 0px
+          margin: 0 auto
+          text-align: center
+          border: 1px solid $color-theme
+          color: $color-theme
+          border-radius: 100px
+          font-size: 0
+          .icon-play
+            display: inline-block
+            vertical-align: middle
+            margin-right: 6px
+            font-size: $font-size-medium-x
+          .text
+            display: inline-block
+            vertical-align middle
+            font-size: $font-size-small
+      .filter
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        background: rgba(7, 17, 27, 0.4)
+    .bg-layer
+      position: relative
+      height: 100%
+      background: $color-background
+    .list
+      position: absolute
+      top: 0
+      bottom: 0
+      width: 100%
+      background: $color-background
+      .song-list-wrapper
+        padding: 20px 30px
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
